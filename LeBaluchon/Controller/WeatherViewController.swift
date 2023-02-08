d6ba599fb84b69e541fb16035a967afb3c16d297
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-02-08 15:56:41 +0100

    Tab bar added

--- LeBaluchon/Controller/WeatherViewController.swift
+++ LeBaluchon/Controller/WeatherViewController.swift
@@ -6,19 +6,44 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
+    
 
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    let locationManager = CLLocationManager()
+    var longitude = 0.00
+    var latitude = 0.00
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
 
         
     }
     
+}
 
-
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+          
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
